63026d951e86a266d60db28618d1f57094538c74
mateus24 <141456405+Mateusfreitas24@users.noreply.github.com>
2024-11-16 20:48:59 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,30 @@
+criaCartao(
+    "Biologia",
+    "O que é fotossíntese?",
+    " A fotossíntese é o processo pelo qual as plantas, algas e algumas bactérias transformam dióxido de carbono e água em glicose e oxigênio, utilizando a luz solar como fonte de energia."
+)
+criaCartao(
+    "História",
+    "Qual foi a causa principal da Primeira Guerra Mundial?",
+    "A causa principal foi a rivalidade entre as potências europeias, agravada pelo assassinato do arquiduque Francisco Ferdinando, herdeiro do trono austro-húngaro, em 1914."
+)
+criaCartao(
+    "Matemática",
+    "Quantas casas decimais tem o número pi?",
+    " Infinitas."
+)
+criaCartao(
+    "Geografia",
+    "Quais o menor e o maior país do mundo?",
+    "Vaticano e Rússia."
+)
+criaCartao(
+    "Filosofia",
+    "De quem é a famosa frase “Penso, logo existo”?",
+    "filósofo René Descartes"
+)
+criaCartao(
+    "Conhecimentos Gerais",
+    "De onde é a invenção do chuveiro elétrico?",
+    "Brasil"
+)
